45107293f3b7f51594c522bb8ae07d0f26da7246
Rajat Mishra <rajatmishra482001@gmail.com>
2024-02-17 14:53:51 +0530

    Update CartPage component with subtotal, invoice button, and invoice modal.

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DefaultLayout from "../components/DefaultLayout";
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -6,11 +6,12 @@ import {
   PlusSquareOutlined,
   MinusSquareOutlined,
 } from "@ant-design/icons";
-import { Table } from "antd";
+import { Button, Table, Modal, message } from "antd";
 
 const CartPage = () => {
   const { cartItems } = useSelector((state) => state.rootReducer);
-
+  const [subTotal, setSubTotal] = useState(0);
+  const [billPopUp, setBillPopUp] = useState(false);
   const dispatch = useDispatch();
   const handleIncrement = (record) => {
     dispatch({
@@ -72,10 +73,35 @@ const CartPage = () => {
     },
   ];
 
+  useEffect(() => {
+    let temp = 0;
+    cartItems.forEach((item) => (temp += item.price * item.quantity));
+    setSubTotal(temp);
+  }, [cartItems]);
+
   return (
     <DefaultLayout>
       <h1>Cart Page</h1>
       <Table dataSource={cartItems} columns={coloums} bordered />
+      <div className="d-flex flex-column align-items-end">
+        <hr />
+        <h3>
+          Sub Total: <span>&#8377;</span>
+          <b>{subTotal}</b>{" "}
+        </h3>
+        <Button type="primary" onClick={() => setBillPopUp(true)}>
+          Create Invoice
+        </Button>
+      </div>
+      <Modal
+        open={billPopUp}
+        onCancel={() => {
+          setBillPopUp(false);
+        }}
+        footer={false}
+      >
+        <p>Invoice Modal</p>
+      </Modal>
     </DefaultLayout>
   );
 };
